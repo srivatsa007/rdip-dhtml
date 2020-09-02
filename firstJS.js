3274500d2dfe5e3b10bbd3feec82db2d62c23bcf
Srivats <70172502+srivatsa007@users.noreply.github.com>
2020-09-03 00:38:58 +0530

    array element

--- firstJS.js
+++ firstJS.js
@@ -1,7 +1,7 @@
 var myname= "srivatsa";
 myname= "DS SRIVATSA";
 let age= 20;
-const rollNo= 121710406007;
+const rollNo= [121710406007,"GITAMIS07",007];
 console.log(myname);
 console.log(age);
-console.log(rollNo);
+console.log(rollNo[0]);
