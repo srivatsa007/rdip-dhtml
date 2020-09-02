0041d9f317c5549839ab57a42c0467a68e1f42c1
Srivats <70172502+srivatsa007@users.noreply.github.com>
2020-09-03 00:51:11 +0530

    array typecasting

--- firstJS.js
+++ firstJS.js
@@ -1,7 +1,8 @@
 var myname= "srivatsa";
 myname= "DS SRIVATSA";
 let age= 20;
-const rollNo= [121710406007,"GITAMIS07",007];
+var rollNo= [1,2,3,4,5];
+rollNo = string(rollNo[3]) //type casting
 console.log(myname);
 console.log(age);
-console.log(rollNo[0]);
+console.log(rollNo);
